5606670d55cda81987fcd4eff58bf4ad00e863bc
maatheusgois <matheus.gois@btgpactual.com>
2025-01-01 18:11:25 -0300

    Initial Commit

---
+++ HyperionSwift/HyperionSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperionSwiftApp.swift
+//  HyperionSwift
+//
+//  Created by Matheus Gois on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HyperionSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
